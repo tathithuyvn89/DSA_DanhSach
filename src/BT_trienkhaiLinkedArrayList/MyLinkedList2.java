b01509ccade93785e50aaebe644b58cf3180fc67
tathithuyvn89 <61693986+tathithuyvn89@users.noreply.github.com>
2020-04-21 19:30:41 +0700

    DSA

--- src/BT_trienkhaiLinkedArrayList/MyLinkedList2.java
+++ src/BT_trienkhaiLinkedArrayList/MyLinkedList2.java
@@ -1,52 +1,186 @@
 package BT_trienkhaiLinkedArrayList;
 
 import org.w3c.dom.Node;
+import thuchanh_MyLinkedArray.MyLinkedList;
 
 public class MyLinkedList2<E> {
-    private Node head;
+    private Node head=null;
+    private Node tail=null;
     private int numNodes;
-    public MyLinkedList2(Object data){
-        head=  new Node(data);
+    public MyLinkedList2(){
+
     }
-    private class Node {
-        private Node next;
-        private Object data;
-        public Node(Object data){
-            this.data=data;
+
+    private class Node <E> {
+        private Node <E> next;
+        private E element;
+        public Node(E element){
+            this.element=element;
         }
 
-        public Object getData() {
-            return this.data;
+        public E getElement() {
+            return this.element;
         }
-    }
-    public void add (int index, E e){
-        Node temp=head;
-        Node holder;
-        for (int i=0;i<index-1&&temp.next!=null;i++){
-            temp=temp.next;
-        }
-        holder= temp.next;
-        temp.next=new Node(e);
-        temp.next.next=head;
-        numNodes++;
     }
     public void addFist(E e){
-        Node temp=head;
-        head= new Node(e);
-        head.next=temp;
+        Node newNode= new Node(e);
+        newNode.next=head;
+        head= newNode;
+        if (tail==null){
+            tail=head;
+        }
         numNodes++;
     }
     public void addLast(E e){
-        Node holder= new Node(e);
-
+        Node newNode= new Node(e);
+        if (tail==null){
+            head=tail=newNode;
+        } else {
+            tail.next=newNode;
+            tail=tail.next;
+        }
+        numNodes++;
+    }
+    public void add (int index, E e){
+        if (index==0){
+            addFist(e);
+        } else if (index>=numNodes){
+            addLast(e);
+        } else {
+            Node current= head;
+            for (int i=1;i<index;i++){
+                current=current.next;
+            }
+            Node temp=current.next;
+            current.next=new Node(e);
+            (current.next).next=temp;
+            numNodes++;
+        }
+    }
+    public E removeFist(){
+        if (numNodes==0){
+            throw new RuntimeException("Size of list is zero, cannot remove any element");
+        } else {
+            Node temp=head;
+            head=head.next;
+            numNodes--;
+            if (head==null){
+                tail=null;
+            }
+            return (E) temp.element;
+        }
+    }
+    public E removeLast(){
+        if (numNodes==0){
+            throw new RuntimeException("Size of list is zero, cannot remove any element");
+        } else if (numNodes==1){
+            Node temp=head;
+            head=tail=null;
+            numNodes=0;
+            return (E) temp.element;
+        } else {
+            Node current = head;
+            for (int i=0; i<numNodes-2;i++){
+                current =current.next;
+            }
+            Node temp=tail;
+            tail=current;
+            tail.next=null;
+            numNodes--;
+            return (E) temp.element;
+        }
+    }
+    public E remove(int index){
+        if (index<0||index>=numNodes){
+            throw new RuntimeException("Size of list is zero, cannot remove");
+        } else if (index==0){
+            return removeFist();
+        } else if (index==numNodes-1){
+            return removeLast();
+        } else {
+            Node privious=head;
+            for (int i=0;i<index;i++){
+                privious=privious.next;
+            }
+            Node current = privious.next;
+            privious.next=current.next;
+            numNodes--;
+            return (E) current.element;
+        }
+    }
+    public int size(){
+        return numNodes;
     }
     public void printList(){
-        Node temp=head;
+        Node temp= head;
         while (temp!=null){
-            System.out.println(temp.data);
+            System.out.println(temp.element);
             temp=temp.next;
         }
     }
+    public E clone(E arr []){
+        for (int i=0; i<arr.length;i++){
+            addLast( arr[i]);
+        }
+        Node temp=tail;
+      return (E) temp.element;
+    }
+    public boolean contain (E e){
+        if (head.element==e){
+            return true;
+        } else {
+            Node current= head;
+            for (int i=1; i<numNodes-1;i++){
+                current=current.next;
+                if (current.element==e){
+                    return true;
+                }
 
+            }
+            return false;
+        }
 
+    }
+    public void ensureCapacity( int minCapacity){
+        if (numNodes==minCapacity){
+            System.out.println(" Size of list over");
+        }
+    }
+    public int indexOf(E e){
+        int index =-1;
+        if (head.element==e){
+            return index=0;
+        } else {
+            Node current= head;
+            for (int i=1;i<numNodes-1;i++){
+                current=current.next;
+                if (current.element==e){
+                    index=i;
+                }
+
+            }
+            return index;
+        }
+
+    }
+    public void clear(){
+       head=tail=null;
+       numNodes=0;
+    }
+    public E get(int index){
+        if (index<0&&index>=numNodes){
+            throw new RuntimeException(" Have not index at "+ index);
+        }else {
+            if (index==0){
+                return (E)head.element;
+            } else {
+                Node current= head;
+                for (int i=1; i<=index;i++){
+                    current=current.next;
+                }
+                return (E) current.element;
+            }
+        }
+
+    }
 }
--- src/BT_trienkhaiLinkedArrayList/TestLinkedArray.java
+++ src/BT_trienkhaiLinkedArrayList/TestLinkedArray.java
@@ -3,14 +3,29 @@ package BT_trienkhaiLinkedArrayList;
 import thuchanh_MyLinkedArray.MyLinkedList;
 
 public class TestLinkedArray {
-    public static void main(String[] args) {
-        MyLinkedList2 ll= new MyLinkedList2(10);
-        ll.addFist(11);
-        ll.addFist(12);
-        ll.addFist(13);
-        ll.add(4,9);
-        ll.add(5,9);
+    public static void main(String[] args) throws Exception {
+        MyLinkedList2 <Integer>ll= new MyLinkedList2();
+        ll.addFist(2);
+        ll.addFist(3);
+        ll.addFist(5);
+        ll.addLast(6);
+        ll.addLast(7);
+        ll.add(2,100);
         ll.printList();
+        System.out.println("Size of list: "+ll.size());
+        System.out.println("Check remove "+ ll.removeLast());
+        ll.printList();
+        System.out.println("Check remove at index= 3 (element=100)");
+        ll.remove(3);
+        ll.printList();
+        System.out.println(ll.get(4));
+//        System.out.println(ll.contain(100));
+//        System.out.println(ll.indexOf(5));
+
+//        System.out.println("====Clear===");
+//        ll.clear();
+//        ll.printList();
+
     }
 
 }
